aa8b3eb419549b7dca954bb1a1e22053cc59f2a0
Igor Polajžer <igor.polajzer@tridenstechnology.com>
2026-05-12 21:29:59 +0200

    Finished implemenation

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import matplotlib
-import numpy as np
 from importlib import reload
 import util
-from util import generate_frequency, plot_dft_and_fft
 
 reload(util)
 
 if __name__ == '__main__':
     T = 1.0
-    Fs = 1000
-
-    freq = generate_frequency(1.0, 20, 0.0, T, Fs)
-
-    y_dft = util.dft(freq, Fs, T)
-    y_fft = np.fft.fft(freq)
-
-    plot_dft_and_fft(y_fft, T, Fs, "FFT")
-    plot_dft_and_fft(y_fft, T, Fs, "DFT")
+    y_a, Fsa = util.read_signal_from_mp3("/home/igor/Desktop/MAG/1_LETNIK/2_SEMESTER/RACUNALNISKA_OBDELAVA_SIGNALOV_IN_SLIK/Vaja_2/recordings/a_medium_pitch.mp3")
+    util.fft_dft(y_a, Fsa, T)
--- util.py
+++ util.py
@@ -1,5 +1,12 @@
+from pydub import AudioSegment
 import numpy as np
 from matplotlib import pyplot as plt
+
+
+def read_signal_from_mp3(input_file):
+    sound = AudioSegment.from_mp3(input_file)
+    samples = np.frombuffer(sound.raw_data, dtype=np.int16).astype(np.float32) / 32767
+    return samples, sound.frame_rate
 
 
 def generate_frequency(A, freq, p, T, Fs):
@@ -15,26 +22,40 @@ def plot_frequency(A, y, p, t, Fs, freq):
     plt.grid(True)
     plt.show()
 
-def dft(x, Fs, T):
-    N = len(x)
-    n = np.arange(N)
-    freqs = np.arange(0, Fs, 1/T)
+def dft(freq, Fs, T, max_freqs):
+    N = np.arange(len(freq)) # Frequency length.
+
+    # All frequencies from 0 to Fs stepped by 1/T
+    # Freqency resolution 1/T
+    freqs = np.arange(0, Fs, 1/T)[:max_freqs]
     result = []
 
+    # Dot product over all frequencies [0, Fs].
     for f in freqs:
-        X_f = np.dot(x, np.exp(-1j * 2 * np.pi * f * n / Fs))
-        result.append(X_f)
+        result.append(np.dot(freq, np.exp(-1j * 2 * np.pi * f * N / Fs)))
 
     return np.array(result)
 
 
-def plot_dft_and_fft(y_fft, y_dft, T, Fs):
-    t = np.arange(0, T, (1.0 / Fs))
+def real_sin_dft(freq, Fs, T, real_freq, max_freqs):
+    # All frequencies from 0 to Fs stepped by 1/T
+    # Freqency resolution 1/T
+    freqs = np.arange(0, Fs, 1/T)[:max_freqs]
+    result = []
 
-    N = len(t)
-    x = np.linspace(0, Fs, N)
+    # Dot product over all frequencies [0, Fs].
+    for f in freqs:
+        result.append(np.dot(freq, real_freq))
 
-    plt.plot(x, abs(y_dft), label="DFT")
+    return np.array(result)
+
+
+def plot_analasys(y, Fs, T, label):
+    N_total = int(T * Fs)
+    max_freq = (len(y) / N_total) * Fs
+    x = np.linspace(0, max_freq, len(y))
+
+    plt.plot(x, abs(y), label=label)
     plt.title('Frekvenčna vsebina')
     plt.xlabel('Frekvenca [Hz]')
     plt.ylabel('Amplituda')
@@ -42,10 +63,14 @@ def plot_dft_and_fft(y_fft, y_dft, T, Fs):
     plt.grid(True)
     plt.show()
 
-    plt.plot(x, abs(y_fft), label="FFT")
-    plt.title('Frekvenčna vsebina')
-    plt.xlabel('Frekvenca [Hz]')
-    plt.ylabel('Amplituda')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+
+def plot_dft_and_fft(y_fft, y_dft, T1, T2, Fs):
+    plot_analasys(y_fft, Fs, T1, 'FFT')
+    plot_analasys(y_dft, Fs, T2, 'DFT')
+
+
+def fft_dft(freq, Fs, T, max_freqs):
+    y_dft = dft(freq, Fs, T, max_freqs)
+    y_fft = np.fft.fft(freq)[:max_freqs] # We take the first 'max_freqs' found through FFT.
+
+    plot_dft_and_fft(y_fft, y_dft, T, T, Fs)
